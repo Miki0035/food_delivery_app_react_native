29f83dfac323894d3a9af21b39406c75a9cd1f0f
Mikiyas Kebede <mknewkebede@gmail.com>
2025-07-11 21:24:02 +0300

    added route groups , appwrite

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.mikiyas.fooddeliveryapp",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    database: '687153bc0014dbe2fdf0',
+    userCollectionId: '687153f8002f351db633'
+}
